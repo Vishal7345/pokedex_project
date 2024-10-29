536114571ae98121e53c78e859f4d02e1403b2a7
golu <tajne>
2024-10-30 02:39:11 +0530

    addes css to pokedex

--- src/Components/PokemonList/PokemonList.jsx
+++ src/Components/PokemonList/PokemonList.jsx
@@ -47,9 +47,15 @@ function PokemonList() {
     return (
         <div className="pokemon-list-wrapper">
             <div>Pokemon List</div>
+            <div className="pokemon-wrapper">
             {isLoading ? 'Loading...' : 
                 pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} />)
             }
+            </div>
+            <div className="controls">
+                <button>prev</button>
+                <button>next</button>
+            </div>
         </div>
     );
 }
